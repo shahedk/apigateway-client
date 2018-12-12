5cf63bd8d37969daef60b96daad7299fe2a05304
shahedk <shahedk@gmail.com>
2018-12-12 16:55:13 +0600

    init

---
+++ app.js
@@ -0,0 +1,39 @@
+let _apiEndPoint = "";
+let _serviceName = "";
+
+function validateApiKey(req, res, next){
+    
+    if(_apiEndPoint.length === 0 || _serviceName.length === 0){
+        throw new Error("Please initialize apigateway-client and specify 'API end point' and 'service name'");
+    }
+    
+    let apiKey = req.headers.apikey !== undefined ? req.headers.apikey : req.params.apikey;
+    let apiSecret = req.headers.apisecret !== undefined ? req.headers.apisecret : req.params.apisecret;
+    
+    // Check if apiKey and api secrect is available in request
+    if(apiKey === undefined || apiKey.length == 0 || 
+       apiSecret === undefined || apiSecret.length == 0){
+        
+        res.send(401,"apikey and/or apisecret missing");
+    }
+    
+    // Check if ApiKey && ApiSecret is valid
+    //TODO: Replace dummy code with real validation
+    if( apiKey.length > 0 && apiSecret.length > 0){
+        return next();
+    }
+    else{
+        res.send(401, "apikey and/or apisecret is not valid")
+    }
+}
+
+function init(apiEndPoint, serviceName){
+    _apiEndPoint = apiEndPoint;
+    _serviceName = serviceName;
+}
+
+
+module.exports = {
+    validateApiKey : validateApiKey,
+    init : init
+}
